df52c6a2eb830890e9d04a3abca8354fffb2740b
kosuke55 <kosuke.tnp@gmail.com>
2024-09-11 15:23:54 +0900

    fix
    Signed-off-by: kosuke55 <kosuke.tnp@gmail.com>

--- pacmod_interface/src/pacmod_interface/pacmod_interface.cpp
+++ pacmod_interface/src/pacmod_interface/pacmod_interface.cpp
@@ -408,7 +408,8 @@ void PacmodInterface::publishCommands()
   const double desired_steer_wheel = std::invoke([&]() -> double {
     double desired_steer_wheel{0.0};
     if (convert_steer_command_) {
-      (control_cmd_ptr_->lateral.steering_tire_angle - steering_offset_) * adaptive_gear_ratio;
+      desired_steer_wheel =
+        (control_cmd_ptr_->lateral.steering_tire_angle - steering_offset_) * adaptive_gear_ratio;
     } else {
       desired_steer_wheel = actuation_cmd_ptr_->actuation.steer_cmd;
     }
